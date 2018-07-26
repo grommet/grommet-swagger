ba0c956c6a8ee449fd914429c00a7c046385c3b6
karatehcops <contact@lazers.tv>
2018-07-26 18:04:38 -0400

    Add Definition.

---
+++ src/Definition.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Box, Heading, Markdown, Responsive, Text } from 'grommet';
+import Nav from './Nav';
+
+export default class Definition extends Component {
+  state = { responsive: 'wide' };
+
+  render() {
+    const { contextSearch, data, title } = this.props;
+    const { responsive } = this.state;
+    const definitions = title && data.definitions && data.definitions[title];
+    console.log(definitions);
+    return (
+      <Responsive
+        onChange={nextResponsive => this.setState({ responsive: nextResponsive })}
+      >
+        <Box direction='row-responsive' justify='center'>
+          <Box
+            basis={responsive === 'wide' ? 'xlarge' : undefined}
+            flex='shrink'
+            pad='large'
+            style={{ minWidth: 0 }}
+          >
+            <Box pad={{ bottom: 'large' }} border='bottom'>
+              <Heading level={1} margin='none'>{title}</Heading>
+            </Box>
+            <Box pad={{ vertical: 'medium' }}>
+              { definitions && definitions.properties &&
+                Object.keys(definitions.properties).map(property => (
+                  <Box direction='row-responsive' key={property}>
+                    <Heading level={3} size='small' margin='small'>
+                      <strong><code>{property.toString()}</code></strong>
+                    </Heading>
+                    <Box basis='medium' pad={{ right: 'medium' }}>
+                      <Markdown>
+                        {` `} 
+                        {/* (parameter.description || '')
+                          .replace(new RegExp('</BR>', 'gi'), '\n\n') */}
+                      </Markdown>
+                      {/* parameter.type !== 'string' ? <Text color='dark-5'>{parameter.type}</Text> : null */}
+                      {/* parameter.required ? <Text color='dark-5'>required</Text> : null */}
+                    </Box>
+                  </Box>))
+              }
+            </Box>
+          </Box>
+          <Nav contextSearch={contextSearch} data={data} />
+        </Box>
+      </Responsive>
+    );
+  }
+}
+
+Endpoint.propTypes = {
+  contextSearch: PropTypes.string.isRequired,
+  data: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+};
